27194f2bbbc424a83afd18d64318eade0899b137
Damon Kiesow <damon@kiesow.net>
2020-12-11 21:06:32 -0600

    Added config for ZotCollectionNotes.py

--- config_sample.py
+++ config_sample.py
@@ -5,4 +5,10 @@ zotsearchNotes = {
 "secretKey": "getfromZotero",
 "filePath": "/Users/path/to/export/rtf/",
 "searchQuery": "innovation",
+},
+zotcollectionNotes = {
+"userID": "1234",
+"groupID": "5678",
+"secretKey": "getfromZotero",
+"filePath": "/Users/path/to/export/rtf/",
 }
