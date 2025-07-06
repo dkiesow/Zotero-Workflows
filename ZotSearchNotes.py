9cda4152a0b1abe1e5e4d5eb2451c0a3b23cd0ba
kiesowd <damon@kiesow.net>
2025-07-06 11:53:46 -0500

    Ensure UTF-8 encoding for collection names and update config import to use ZOTERO_CONFIGS. Add safe_utf8 function and set ensure_asciiTo match the config import style and structure of your other scripts, update the import to use from config import ZOTERO_CONFIGS, and access the config via ZOTERO_CONFIGS["SearchNotes"] (assuming your config uses this key). Also, use a main() function and proper argument handling.

--- ZotSearchNotes.py
+++ ZotSearchNotes.py
@@ -1,108 +1,98 @@
 #!/usr/bin/env python
-import config as cfg
 from pyzotero import zotero
 import datetime
 import io
 import sys
 import re
+from config import ZOTERO_CONFIGS
 
-userID = cfg.zotSearchNotes["userID"]
-secretKey = cfg.zotSearchNotes["secretKey"]
-filePath = cfg.zotSearchNotes["filePath"]
-searchQuery = cfg.zotSearchNotes["searchQuery"]
+def main():
+    zot_config = ZOTERO_CONFIGS["SearchNotes"]
+    user_id = zot_config["userID"]
+    secret_key = zot_config["secretKey"]
+    file_path = zot_config["filePath"]
+    default_query = zot_config["searchQuery"]
 
-# Comment out the next line to test using the searchterm in config.py
-searchQuery = sys.argv[1]
+    # Use CLI argument if provided, else fallback to config
+    search_query = sys.argv[1] if len(sys.argv) > 1 else default_query
 
-zot = zotero.Zotero(userID, 'user', secretKey, 'preserve_json_order = true')
-# we now have a Zotero object, zot, and access to all its methods
+    zot = zotero.Zotero(user_id, 'user', secret_key, preserve_json_order=True)
 
-collectionsListKeys = {}
-parentID = {}
-searchResult = zot.top(q=searchQuery, qmode="everything")
+    search_result = zot.top(q=search_query, qmode="everything")
+    # Remove stray itemtypes 'attachment'
+    search_result = [item for item in search_result if item['data']['itemType'] != 'attachment']
 
-# remove stray itemtypes 'attachment' that may be top level by accident
-indices = [i for i, n in enumerate(searchResult) if n['data']['itemType'] == 'attachment']
-searchResult[:] = [j for i, j in enumerate(searchResult)
-                   if i not in indices]
-noteItems = []
-i = 0
-for i in range(len(searchResult)):
-    childHold = zot.children(searchResult[i]['key'])
-    for x in childHold:
-        if "note" in x['data']['itemType']:
-            noteItems.append(x['data'])
+    note_items = []
+    for item in search_result:
+        for child in zot.children(item['key']):
+            if "note" in child['data']['itemType']:
+                note_items.append(child['data'])
 
-# remove notes with 'the following values have no...'
-indices = [i for i, n in enumerate(noteItems) if n['note'].startswith('The following values')]
-noteItems[:] = [j for i, j in enumerate(noteItems)
-                if i not in indices]
+    # Remove notes with 'the following values have no...'
+    note_items = [n for n in note_items if not n['note'].startswith('The following values')]
 
-# create a list of collection keys for later use
-collectionsListKeys = {}
-collectionsInfo = zot.collections()
-i = 0
-for i in range(len(collectionsInfo)):
-    collectionsListKeys[(collectionsInfo[i]['data']['key'])] = dict(
-        {'Name': collectionsInfo[i]['data']['name'], 'Parent': collectionsInfo[i]['data']['parentCollection']})
+    # Build collection keys
+    collections_info = zot.collections()
+    collections_list_keys = {
+        col['data']['key']: {
+            'Name': col['data']['name'],
+            'Parent': col['data']['parentCollection']
+        }
+        for col in collections_info
+    }
 
-# build the body of the file
-notes = []
-notesHold = []
-collectionParentID = []
-i = 0
-for i in range(len(noteItems)):
-    notesHold = (noteItems[i])
-    notesRaw = notesHold['note']
-    if notesRaw.startswith('<p><strong>Extracted Annotations') or notesRaw.startswith('<p><b>Extracted Annotations'):
-        parentID = notesHold['parentItem']
-        '''ID of Parent Document'''
-        parentDoc = zot.item(parentID)
-        '''Full data for Parent Document'''
-        match = re.search(r"(?<!\d)\d{4,20}(?!\d)", parentDoc['data']['date'])
-        parentDate = match.group(0) if match else None
-        '''Get publication date for Parent Document'''
-        collectionID = parentDoc['data']['collections'][0]
-        '''Get collectionID for Parent Document'''
-        collectionParentID = collectionsListKeys[collectionID]['Parent']
-        '''Get ID for Parent Collection'''
-        if not str(collectionParentID):
-            '''Minor error catching/branch if the collection is the parent'''
-            parentCollectionName = collectionsListKeys[collectionParentID]['Name']
-            breadCrumb = parentCollectionName + "/" + collectionsListKeys[collectionID]['Name']
-            collectionTitle = parentCollectionName
-        else:
-            breadCrumb = collectionsListKeys[collectionID]['Name']
-            collectionName = breadCrumb
-            collectionTitle = collectionName
-            parentTitle = parentDoc['data']['title']
-            parentCreators = parentDoc['meta']['creatorSummary']
-        if not notesRaw:
-            package = "\\i " + "No Notes"
+    notes = []
+    for note in note_items:
+        notes_raw = note['note']
+        if notes_raw.startswith('<p><strong>Extracted Annotations') or notes_raw.startswith('<p><b>Extracted Annotations'):
+            parent_id = note['parentItem']
+            parent_doc = zot.item(parent_id)
+            match = re.search(r"(?<!\d)\d{4,20}(?!\d)", parent_doc['data']['date'])
+            parent_date = match.group(0) if match else ""
+            collection_id = parent_doc['data']['collections'][0]
+            collection_parent_id = collections_list_keys[collection_id]['Parent']
+            if collection_parent_id and str(collection_parent_id) in collections_list_keys:
+                parent_collection_name = collections_list_keys[collection_parent_id]['Name']
+                bread_crumb = parent_collection_name + "/" + collections_list_keys[collection_id]['Name']
+                collection_title = parent_collection_name
+            else:
+                bread_crumb = collections_list_keys[collection_id]['Name']
+                collection_title = bread_crumb
+            parent_title = parent_doc['data']['title']
+            parent_creators = parent_doc['meta']['creatorSummary']
+            if not notes_raw:
+                package = "\\i No Notes"
+            else:
+                package = (
+                    "\\i " + bread_crumb + "\\i0 \\line " +
+                    "\\fs28 \\b " + parent_title + " (" + parent_date + ") \\b0 \\fs22 \\line " +
+                    parent_creators + " \\line \\fs24 " + notes_raw
+                )
             notes.append(package)
-        else:
-            package = "\\i " + str(
-                breadCrumb) + "\\i0 \\line " + "\\fs28 \\b " + parentTitle + " (" + parentDate + ") " + " \\b0 \\fs22 \\line " + parentCreators + " \\line \\fs24 " + notesRaw
-            notes.append(package)
-            '''Some concatenation and appending to the overall file'''
 
-output = "\\par".join(notes)
-# various translations for RTF and quick replacement for characters that don't encode correctly
-output = output.replace("(<a href=", "{\\field{\\*\\fldinst { HYPERLINK")
-output = output.replace("\">", "}}{\\fldrslt {")
-output = output.replace("</a>)", "}}}")
-output = output.replace("<p>", "\\line")
-output = output.replace("</p>", "\\line")
-output = output.replace("<strong>", "\\b ")
-output = output.replace("</strong>", " \\b0")
-output = output.replace("\u02d8", "&#728;")
-output = output.replace("\u02C7", "&#728;")
+    output = "\\par".join(notes)
+    # RTF and character replacements
+    output = output.replace("(<a href=", "{\\field{\\*\\fldinst { HYPERLINK")
+    output = output.replace("\">", "}}{\\fldrslt {")
+    output = output.replace("</a>)", "}}}")
+    output = output.replace("<p>", "\\line")
+    output = output.replace("</p>", "\\line")
+    output = output.replace("<strong>", "\\b ")
+    output = output.replace("</strong>", " \\b0")
+    output = output.replace("\u02d8", "&#728;")
+    output = output.replace("\u02C7", "&#728;")
 
-timestamp = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
-rtf = "{\\rtf1\\ansi\\ansicpg1252\\deff0\\deftab720{\\fonttbl{\\f0\\fswiss MS Sans Serif;}{\\f1\\froman\\fcharset2 Symbol;}{\\f2\\fmodern\\fprq1 Courier New;}{\\f3\\froman Times New Roman;}}{\\colortbl\\red0\\green0\\blue0;\\red0\\green0\\blue255;\\red255\\green0\\blue0;}\\deflang1033\\horzdoc{\\*\\fchars }{\\*\\lchars}"
-f = io.open(filePath + searchQuery + '_Zotero_notes_' + timestamp + '.rtf',
-            'w+', encoding="cp1252", )
-f.write(rtf)
-f.write(output + "\par")
-f.write("}")
-f.close()
+    timestamp = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
+    rtf = (
+        "{\\rtf1\\ansi\\ansicpg1252\\deff0\\deftab720{\\fonttbl{\\f0\\fswiss MS Sans Serif;}"
+        "{\\f1\\froman\\fcharset2 Symbol;}{\\f2\\fmodern\\fprq1 Courier New;}"
+        "{\\f3\\froman Times New Roman;}}{\\colortbl\\red0\\green0\\blue0;"
+        "\\red0\\green0\\blue255;\\red255\\green0\\blue0;}\\deflang1033\\horzdoc{\\*\\fchars }{\\*\\lchars}"
+    )
+    with io.open(f"{file_path}{search_query}_Zotero_notes_{timestamp}.rtf", 'w+', encoding="cp1252") as f:
+        f.write(rtf)
+        f.write(output + "\\par")
+        f.write("}")
+
+if __name__ == "__main__":
+    main()
